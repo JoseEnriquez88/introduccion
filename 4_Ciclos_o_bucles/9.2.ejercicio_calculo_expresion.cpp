84e713e25a32e592ef1d210d31a96dddf4495ac5
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-07-05 09:39:02 -0300

    bucles update

--- 4_Ciclos_o_bucles/9.2.ejercicio_calculo_expresion.cpp
+++ 4_Ciclos_o_bucles/9.2.ejercicio_calculo_expresion.cpp
@@ -1,31 +1,33 @@
 /*12. Hacer un programa que calcule el resultado de la siguiente expresion:
 1-2+3-4+5-6...n*/
 
-#include<iostream>
-#include<stdlib.h>
-
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
 
-int main(){
-	int n,suma=0,par=0,impar=0,negativo=0;
-	
-	cout<<"Digite el numero de elementos: ";
-	cin>>n;
-	
-	for(int i=1;i<=n;i++){
-		if(i%2==0){//los numeros pares son negativos
-			negativo = i * -1; //los cambiamos de signo
-			par += negativo;			
+int main()
+{
+	int n, suma = 0, par = 0, impar = 0, negativo = 0;
+
+	cout << "Digite el numero de elementos: ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			negativo = i * -1;
+			par += negativo;
 		}
-		else{ //los impares son positivos
+		else
+		{
 			impar += i;
 		}
 	}
-	
+
 	suma = par + impar;
-	
-	cout<<"\nLa suma es: "<<suma<<endl;
+
+	cout << "\nLa suma es: " << suma << endl;
 	system("pause");
 	return 0;
 }
-

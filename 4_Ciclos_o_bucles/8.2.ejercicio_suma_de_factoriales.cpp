848465a12102c91ffac88831e43811ad9ce386e6
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-07-05 09:26:40 -0300

    bucles updated

--- 4_Ciclos_o_bucles/8.2.ejercicio_suma_de_factoriales.cpp
+++ 4_Ciclos_o_bucles/8.2.ejercicio_suma_de_factoriales.cpp
@@ -1,24 +1,25 @@
 // Escriba un programa que calcule el valor de: 1!+2!+3!+...+n! (suma de factoriales).
 
 #include <iostream>
+#include <stdlib.h>
+
 using namespace std;
 
 int main()
 {
-  int numero;
-  long long sumaFactoriales = 0;
-  long long factorial = 1;
+  int numero, suma = 0, factorial = 1;
 
-  cout << "Ingrese un numero: ";
+  cout << "Digite el total de elementos a sumar: ";
   cin >> numero;
 
-  for (int i = 1; i <= numero; ++i)
+  for (int i = 1; i <= numero; i++)
   {
     factorial *= i;
-    sumaFactoriales += factorial;
+    suma += factorial;
   }
 
-  cout << "La suma de los factoriales desde 1 hasta " << numero << " es: " << sumaFactoriales << endl;
+  cout << "\nLa suma de factoriales es: " << suma << endl;
 
+  system("pause");
   return 0;
 }

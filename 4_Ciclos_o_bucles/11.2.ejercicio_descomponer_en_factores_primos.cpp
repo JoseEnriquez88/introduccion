2b789ccbd3aef1ae23c42398d913493f7d5e5a86
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-07-05 09:39:13 -0300

    bucles updated

--- 4_Ciclos_o_bucles/11.2.ejercicio_descomponer_en_factores_primos.cpp
+++ 4_Ciclos_o_bucles/11.2.ejercicio_descomponer_en_factores_primos.cpp
@@ -1,29 +1,32 @@
-/*16. Realice un programa que calcule la descomposición en factores primos de 
-un número entero.
+/*16. Realice un programa que calcule la descomposiciï¿½n en factores primos de
+un nï¿½mero entero.
 
 Por ejemplo: 20 = 2*2*5.
 */
 
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
-int main(){
+int main()
+{
 	int numero;
-	
-	cout<<"Digite un numero: "; 
-	cin>>numero;
-	
-	cout<<"\nDescomponiendo en factores primos: ";
-	for(int i=2;numero>1;i++){
-		while(numero%i==0){
-			cout<<i<<" ";
+
+	cout << "Digite un numero: ";
+	cin >> numero;
+
+	cout << "\nDescomponiendo en factores primos: ";
+	for (int i = 2; numero > 1; i++)
+	{
+		while (numero % i == 0)
+		{
+			cout << i << " ";
 			numero /= i;
 		}
 	}
-	
-	cout<<"\n";
+
+	cout << "\n";
 	system("pause");
 	return 0;
 }

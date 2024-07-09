dda57c9180121fd4d2773ed74e7426b7919150f7
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-07-09 15:05:47 -0300

    arreglos updated

---
+++ 5_Arreglos/3.2.determinar_si_un_numero_en_un_arreglo_equivale_a_la_suma_de_los_demas.cpp
@@ -0,0 +1,36 @@
+/*6. Escribe un programa que defina un vector de n�meros y calcule si existe
+alg�n n�mero en el vector cuyo valor equivale a la suma del resto de n�meros
+del vector.*/
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int numeros[5] = {1, 2, 3, 4, 10}; // Definimos un arreglo
+	int suma = 0, mayor = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		suma += numeros[i];
+
+		if (numeros[i] > mayor)
+		{											// Determinamos cual es el numero mayor, ya que
+			mayor = numeros[i]; // el unico numero que puede ser la suma de los
+		} // demas es el numero mayor
+	}
+
+	if (mayor == suma - mayor)
+	{ // comparamos si el numero mayor es igual a suma menos el numero mayor
+		cout << "El numero " << mayor << " es la suma de los demas";
+	}
+	else
+	{
+		cout << "No existe ningun numero que sea la suma de los demas";
+	}
+
+	getch();
+	return 0;
+}

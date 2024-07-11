2b5893ae52b4499c327adfe510e9c886f8bd3cd8
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-07-11 15:32:43 -0300

    busquedad added

---
+++ 9_Busqueda/1.1.busqueda_secuencial.cpp
@@ -0,0 +1,38 @@
+// Busqueda secuencial
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+  int a[] = {3, 4, 2, 1, 5};
+  int i, dato;
+  char band = 'F';
+
+  dato = 4; // => este es el numero a buscar
+  i = 0;
+  {
+    while (band == 'F' && i < 5)
+    {
+      if (a[i] == dato)
+      {
+        band = 'V';
+      }
+      i++;
+    }
+
+    if (band == 'F')
+    {
+      cout << "El numero a buscar no existe en el arreglo.";
+    }
+    else if (band == 'V')
+    {
+      cout << "El numero a sido encontrado en la pos: " << i - 1 << endl;
+    }
+
+    cout << endl;
+    system("pause");
+    return 0;
+  }
+}

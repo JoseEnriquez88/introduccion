29e8bc88802cb04fbeb2eb79b177d71d50f8cbe1
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-07-29 18:38:45 -0300

    punteros updtd

---
+++ 12_Punteros/7.1.transimision_de_arreglos.cpp
@@ -0,0 +1,33 @@
+// Transimision de arreglos
+
+// Ejemplo => hallar el amximo de elemento de un arreglo
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int hallarMax(int *, int);
+
+int main()
+{
+  const int nElementos = 5;
+  int numeros[nElementos] = {3, 5, 2, 8, 1};
+
+  cout << "El mayor elemento es: " << hallarMax(numeros, nElementos);
+
+  return 0;
+}
+
+int hallarMax(int *dirVec, int nElementos)
+{
+  int max = 0;
+
+  for (int i = 0; i < nElementos; i++)
+  {
+    if (*(dirVec + i) > max)
+    {
+      max = *(dirVec + i);
+    }
+  }
+  return max;
+};
